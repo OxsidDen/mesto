12c28dd7a2e1b7d9c4c68a3a21ba8503d97574fa
Den <hazheev.D@yandex.ru>
2022-10-16 23:07:06 +0300

    fixed:Исправлены замечани

--- script/script.js
+++ script/script.js
@@ -5,19 +5,19 @@ let profileName = document.querySelector('.profile__name');
 let profilAbout = document.querySelector('.profile__about');
 let nameInput = document.querySelector('#popupName');
 let aboutInput = document.querySelector('#popupAbout');
-function ClosePopup(){  // функция для закрытия формы
+function closePopup(){  // функция для закрытия формы
     popup.classList.remove('popup_opend');
 }
-function FormSubmit(evt) { //функция сохранения данных полученных от пользователя
+function formSubmit(evt) { //функция сохранения данных полученных от пользователя
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profilAbout.textContent = aboutInput.value;
-    ClosePopup();
+    closePopup();
 }
 profileChange.addEventListener('click', function(){ // открытие формы для изменения профиля
     popup.classList.add('popup_opend');
     nameInput.value = profileName.textContent;
     aboutInput.value = profilAbout.textContent;
 })
-popupClose.addEventListener('click',ClosePopup ); //само закрытие формы
-popup.addEventListener('submit', FormSubmit); //перенос данных из формы в профиль
+popupClose.addEventListener('click',closePopup ); //само закрытие формы
+popup.addEventListener('submit', formSubmit); //перенос данных из формы в профиль
