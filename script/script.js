5b58748124034707c64f8fc1a2573c205882f4a5
Den <hazheev.D@yandex.ru>
2022-11-08 19:55:14 +0300

    fixed: функция закрытия и открытия попапов, Общаяя разметка попапов

--- script/script.js
+++ script/script.js
@@ -1,10 +1,12 @@
-const popupRejion = document.querySelector('.popup'); // Весь попап (грид сетка и затемнение)
 const popupProfile = document.querySelector('#popupProf'); // попап для изменения профиля
 const popupPlace = document.querySelector('#popupPlace'); // попап для добавления нового поста
 const popupView = document.querySelector('#popupView'); // попап для просмотра постов 
+const formProfile = document.querySelector('#formProf'); // попап для просмотра постов 
+const formPlace = document.querySelector('#formPlace'); // попап для просмотра постов 
+const formView = document.querySelector('#formView'); // попап для просмотра постов 
 const profileChange = document.querySelector('.profile__change'); // кнопка для изменения профиля
 const profileAdd = document.querySelector('.profile__add'); // кнопка для добавления поста
-const popupClose = popupRejion.querySelector('.popup__close-button'); // кнопка закрытия попапа
+const popupClose = Array.from(document.querySelectorAll('.popup__close-button')); // кнопка закрытия попапа
 const profileName = document.querySelector('.profile__name'); // Имя в профиля
 const profilAbout = document.querySelector('.profile__about'); // Род деятельности в профиле
 const nameInput = document.querySelector('#popupName'); // Инпут для имени
@@ -58,9 +60,9 @@ function createCard(item) {
         elementDelete.parentNode.remove();
     });
     elementView.addEventListener('click', () => {
-        openPopup(popupRejion);
         openPopup(popupView);
-        popupRejion.classList.add('popup_view-place');
+        openPopup(formView);
+        
         popupImg.src = elementImg.src;
         popupCaption.textContent = elementParagraph.textContent;
     });
@@ -70,62 +72,59 @@ function createCard(item) {
 function renderCard(elementUnit){
     const card = createCard(elementUnit);
     elements.prepend(card);
-}
+};
+
 initialCards.forEach(renderCard);// Добавление карточек с помощью Js 
 const elemental = document.querySelectorAll('.element'); // NodeList из всех постов
 const elementArray = Array.from(elemental); // Перевод из NodeList в Array
 
 function closePopup(elem){  // функция для закрытия формы
     elem.classList.remove('popup_opend');
-    elem.classList.remove('popup_view-place');
-}
+};
 
 function openPopup(elem){
     elem.classList.add('popup_opend');
-}
+};
 
 function saveSubmitProfile(evt) { //функция сохранения данных полученных от пользователя
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profilAbout.textContent = aboutInput.value;
-    closePopup(popupRejion);
     closePopup(popupProfile);
-}
+    closePopup(formProfile);
+};
 
 function saveSubmitPlace(evt) { //функция сохранения данных полученных от пользователя
     evt.preventDefault();
-    const newCards = [
-        {
-            name: '',
-            link: ''
-        }
-    ]
     if(placeLink.value != '' || placeName.value != ''){
-       newCards.link = placeLink.value;
-       newCards.name = placeName.value;
-       renderCard(newCards);
+        const newCards ={
+            name: placeName.value,
+            link: placeLink.value,
+        };
+    renderCard(newCards);
     }
-    closePopup(popupRejion);
+    closePopup(formPlace);
     closePopup(popupPlace);
-}
+};
 
 profileChange.addEventListener('click', function(){ // открытие формы для изменения профиля
-    openPopup(popupRejion);
+    openPopup(formProfile);
     openPopup(popupProfile);
     nameInput.value = profileName.textContent;
     aboutInput.value = profilAbout.textContent;
-})
-
-profileAdd.addEventListener('click', function(){ // открытие формы для добовлания постов
-    openPopup(popupRejion);
-    openPopup(popupPlace);
 });
 
-popupClose.addEventListener('click', () => {
-        closePopup(popupRejion);
+profileAdd.addEventListener('click', function(){ // открытие формы для добовлания постов
+    openPopup(popupPlace);
+    openPopup(formPlace);
+});
+
+popupClose.forEach((pop) => {
+    pop.addEventListener('click', () => {
         closePopup(popupPlace);
         closePopup(popupProfile);
         closePopup(popupView);
     }); //само закрытие формы
+});
 popupProfile.addEventListener('submit', saveSubmitProfile); //перенос данных из формы в профиль
 popupPlace.addEventListener('submit',saveSubmitPlace); //перенос данных из формы в посты
