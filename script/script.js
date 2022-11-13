b0c3adf43ef9343949e96c912ac712a05fb8e66d
Den <hazheev.D@yandex.ru>
2022-11-14 02:35:14 +0500

    feat: Добавлена валидация и проверка данных

--- script/script.js
+++ script/script.js
@@ -1,23 +1,24 @@
+const popupArray = Array.from(document.querySelectorAll('.popup'));
 const popupProfile = document.querySelector('#popupProf'); // попап для изменения профиля
 const popupPlace = document.querySelector('#popupPlace'); // попап для добавления нового поста
 const popupView = document.querySelector('#popupView'); // попап для просмотра постов 
-const formProfile = document.querySelector('#formProf'); // попап для просмотра постов 
-const formPlace = document.querySelector('#formPlace'); // попап для просмотра постов 
-const formView = document.querySelector('#formView'); // попап для просмотра постов 
+const formProfile = popupProfile.querySelector('#formProf'); // попап для просмотра постов 
+const formPlace = popupPlace.querySelector('#formPlace'); // попап для просмотра постов 
+const formView = popupView.querySelector('#formView'); // попап для просмотра постов 
 const profileChange = document.querySelector('.profile__change'); // кнопка для изменения профиля
 const profileAdd = document.querySelector('.profile__add'); // кнопка для добавления поста
-const popupClose = Array.from(document.querySelectorAll('.popup__close-button')); // кнопка закрытия попапа
+const popupCloseArray = Array.from(document.querySelectorAll('.popup__close-button')); // кнопка закрытия попапа
 const profileName = document.querySelector('.profile__name'); // Имя в профиля
 const profilAbout = document.querySelector('.profile__about'); // Род деятельности в профиле
-const nameInput = document.querySelector('#popupName'); // Инпут для имени
-const aboutInput = document.querySelector('#popupAbout'); // Инпут для деятельности
-const placeName = document.querySelector('#popupPost'); // Инпут для названия нового поста
-const placeLink = document.querySelector('#popupLink'); // Инпут для ссылки на новый пост
-const popupImg = document.querySelector('#viewImg'); // Увеличенная картинка в попапе
-const popupCaption = document.querySelector('.popup__caption'); // Описание карнки в попапе
+const nameInput = formProfile.querySelector('#form__name'); // Инпут для имени
+const aboutInput = formProfile.querySelector('#form__about'); // Инпут для деятельности
+const placeName = formPlace.querySelector('#form__place'); // Инпут для названия нового поста
+const placeLink = formPlace.querySelector('#form__link'); // Инпут для ссылки на новый пост
+const popupImg = formView.querySelector('#viewImg'); // Увеличенная картинка в попапе
+const popupCaption = formView.querySelector('.popup__caption'); // Описание карнки в попапе
 const elementTemplate = document.querySelector('#element').content; // Получение элементов из Template
 const elements = document.querySelector('.elements'); // Отсек со всеми постами
-const initialCards =[
+const firstCards =[
     {
         name: 'Бауманска станция',
         link: './images/Бауманска.jpg'
@@ -43,13 +44,13 @@ const initialCards =[
         link: './images/Орленок.jpg'
     }
 ];
-function createCard(item) {
+function createCard(item, config) {
     const element = elementTemplate.querySelector('.element').cloneNode(true);
     const elementParagraph = element.querySelector('.element__paragraph'); // выбор элементов нового поста
     const elementImg = element.querySelector('.element__img');
     const elementDelete = element.querySelector('.element__delete');
     const elementLike = element.querySelector('.element__like');
-    const elementView = element.querySelector('.element__img-popup');
+    const elementView = element.querySelector('.element__img-popup')
     elementParagraph.textContent = item.name;
     elementImg.src = item.link;
     elementImg.alt = item.name;
@@ -73,9 +74,7 @@ function renderCard(elementUnit){
     elements.prepend(card);
 };
 
-initialCards.forEach(renderCard);// Добавление карточек с помощью Js 
-const elemental = document.querySelectorAll('.element'); // NodeList из всех постов
-const elementArray = Array.from(elemental); // Перевод из NodeList в Array
+firstCards.forEach(renderCard);// Добавление карточек с помощью Js 
 
 function closePopup(elem){  // функция для закрытия формы
     elem.classList.remove('popup_opend');
@@ -83,16 +82,17 @@ function closePopup(elem){  // функция для закрытия формы
 
 function openPopup(elem){
     elem.classList.add('popup_opend');
+
 };
 
-function saveSubmitProfile(evt) { //функция сохранения данных полученных от пользователя
+function saveSubmitProfile(evt) { //функция сохранения данных полученных от пользователя при изменении профиля
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profilAbout.textContent = aboutInput.value;
     closePopup(popupProfile);
 };
 
-function saveSubmitPlace(evt) { //функция сохранения данных полученных от пользователя
+const saveSubmitPlace = (evt) => { //функция сохранения данных полученных от пользователя при добовления новой карточки
     evt.preventDefault();
     if(placeLink.value != '' || placeName.value != ''){
         const newCards ={
@@ -104,6 +104,7 @@ function saveSubmitPlace(evt) { //функция сохранения данны
     closePopup(popupPlace);
 };
 
+
 profileChange.addEventListener('click', function(){ // открытие формы для изменения профиля
     openPopup(popupProfile);
     nameInput.value = profileName.textContent;
@@ -114,12 +115,28 @@ profileAdd.addEventListener('click', function(){ // открытие формы 
     openPopup(popupPlace);
 });
 
-popupClose.forEach((pop) => {
+popupCloseArray.forEach((pop) => {
+    const popupOpened = pop.closest("div");
     pop.addEventListener('click', () => {
-        closePopup(popupPlace);
-        closePopup(popupProfile);
-        closePopup(popupView);
+        closePopup(popupOpened);
     }); //само закрытие формы
 });
+
+document.addEventListener('keydown', (e) =>{
+    if(e.key === "Escape"){
+        const popupOpened = document.querySelector('.popup_opend');
+        closePopup(popupOpened);
+    }
+});
+
+popupArray.forEach((pop) => {
+    const childElement = pop.firstElementChild;
+    pop.addEventListener('click', (evt) =>{
+        if(!(evt.target == childElement || childElement.contains(evt.target))){
+            closePopup(pop);
+        }
+    });
+});
+
 popupProfile.addEventListener('submit', saveSubmitProfile); //перенос данных из формы в профиль
 popupPlace.addEventListener('submit',saveSubmitPlace); //перенос данных из формы в посты
